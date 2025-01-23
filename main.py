ee32f3bb0ecc64f43506621606618bc42fc19e28
alloygoh <106863675+alloygoh@users.noreply.github.com>
2025-01-23 19:25:22 +0800

    feat: make vectorestore persistent & improve prompting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from os import environ, getenv
 from sys import argv
 
+from chromadb.errors import InvalidCollectionException
 from dotenv import load_dotenv
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -15,23 +16,94 @@ from langchain_core.vectorstores import VectorStore
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
+@dataclass
+class CweMapping:
+    vulnerability: str
+    cwe_ids: list[str]
+    scores: list[float]
+    docs: list[Document]
+
+
 def format_docs(docs: list[Document]) -> str:
     return "\n\n".join(doc.page_content for doc in docs)
+
+
+def load_cwe_data(cwe_file_path: str) -> list[dict[str, str]]:
+    with open(cwe_file_path, "r") as file:
+        cwe_data = json.load(file)
+        return [
+            {"id": cwe["ID"], "description": cwe["Description"]} for cwe in cwe_data
+        ]
+
+
+def build_cwe_vectorstore(
+    cwe_data: list[dict[str, str]], embedding_model: OpenAIEmbeddings
+) -> Chroma:
+    descriptions = [f"{cwe['id']}: {cwe['description']}" for cwe in cwe_data]
+    metadata = [{"CWE_ID": cwe["id"]} for cwe in cwe_data]
+
+    vectorstore = Chroma.from_texts(
+        texts=descriptions,
+        embedding=embedding_model,
+        metadatas=metadata,
+        collection_metadata={"hnsw:space": "cosine"},
+        persist_directory="./chroma_db",
+        collection_name="cwe",
+    )
+    return vectorstore
+
+
+def load_cwe_vectorstore(embedding: OpenAIEmbeddings):
+    vectorstore = Chroma(
+        embedding_function=embedding,
+        persist_directory="./chroma_db",
+        collection_name="cwe",
+        create_collection_if_not_exists=False,
+    )
+    if len(vectorstore.get()["ids"]) == 0:
+        raise InvalidCollectionException
+    return vectorstore
+
+
+def get_cwe_vectorstore(embeddings: OpenAIEmbeddings) -> Chroma | None:
+    if create_vector_db:
+        print("loading cwe data...")
+        cwe_data = load_cwe_data("cwe_dict_clean.json")
+        print("building cwe vectorstore...")
+        return build_cwe_vectorstore(cwe_data, embeddings)
+    try:
+        return load_cwe_vectorstore(embeddings)
+    except InvalidCollectionException:
+        return None
+
+
+def map_vulnerabilities_to_cwe(
+    vulnerabilities: list[str], vectorstore: VectorStore, k: int = 1
+) -> list[CweMapping]:
+    mapping_results: list[CweMapping] = []
+    for vuln in vulnerabilities:
+        results = vectorstore.similarity_search_with_score(vuln, k=k)
+        current_mapping = CweMapping(vulnerability=vuln, cwe_ids=[], docs=[], scores=[])
+        if results:
+            for result in results:
+                current_mapping.cwe_ids.append(result[0].metadata["CWE_ID"])
+                current_mapping.scores.append(result[1])
+                current_mapping.docs.append(result[0])
+        mapping_results.append(current_mapping)
+    return mapping_results
 
 
 # load env vars from .env file
 load_dotenv()
 
-hf_token = getenv("HUGGINGFACE_TOKEN")
+create_vector_db = getenv("CREATE_VECTOR_DB") == "TRUE"
 
 if "OPENAI_API_KEY" not in environ:
-    print("Please set the OPENAI_API_KEY|HUGGINGFACE_TOKEN environment variable.")
+    print("Please set the OPENAI_API_KEY environment variable.")
     exit()
 
 llm = ChatOpenAI(model="gpt-4o")
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-# embeddings = OllamaEmbeddings(model="mxbai-embed-large", base_url=BASE_URL)
-
 
 # load PDF
 # TODO: find better parsers and tokeniser or what not
@@ -62,6 +134,7 @@ question = """
 You are a threat intelligence analyst. Look at the breach report provided.
 what are the vulnerable business process or vulnerabilities identified that lead to a compromise? Be as specific as possible.
 You should state each vulnerability in a sentence, and provide a short description not exceeding 1 sentence.
+if the vulnerability stems from outdated software, state that it is a dependency on a vulnerable third-party component.
 Do not repeat the vulnerabilities and do not provide any additional information.
 You should list each vulnerabilities in a numerical order.
 """
@@ -69,72 +142,20 @@ response = rag_chain.invoke({"input": question})
 print(response["answer"])
 
 
-# end of prompt 1
-
 # parse previous response
 response_parsed = response["answer"].split("\n")
 # get
-response_parsed = [i.split(".", 1)[1] for i in response_parsed if len(i) > 0 and i[0].isdigit()]
+response_parsed = [
+    i.split(".", 1)[1] for i in response_parsed if len(i) > 0 and i[0].isdigit()
+]
 print(f"[-] response_parsed: {response_parsed}")
 
-
-# Step 1: Load CWE Data
-def load_cwe_data(cwe_file_path: str):
-    with open(cwe_file_path, "r") as file:
-        cwe_data = json.load(file)
-        return [
-            {"id": cwe["ID"], "description": cwe["Description"]} for cwe in cwe_data
-        ]
-
-
-# Step 2: Build CWE VectorStore
-def build_cwe_vectorstore(cwe_data, embedding_model):
-    descriptions = [f"{cwe['id']}: {cwe['description']}" for cwe in cwe_data]
-    metadata = [{"CWE_ID": cwe["id"]} for cwe in cwe_data]
-
-    vectorstore = Chroma.from_texts(
-        texts=descriptions,
-        embedding=embedding_model,
-        metadatas=metadata,
-        collection_metadata={"hnsw:space": "cosine"},
-    )
-    # vectorstore = InMemoryVectorStore.from_texts(
-    #     texts=descriptions,
-    #     embedding=embedding_model,
-    #     metadatas=metadata,
-    # )
-    return vectorstore
-
-@dataclass
-class CweMapping:
-    vulnerability: str
-    cwe_ids: list[str]
-    scores: list[float]
-    docs: list[Document]
-
-
-# Step 3: Map Vulnerabilities to CWE IDs
-def map_vulnerabilities_to_cwe(
-    vulnerabilities: dict[str, str], vectorstore: VectorStore, k=1
-) -> list[CweMapping]:
-    mapping_results = []
-    for vuln in vulnerabilities:
-        results = vectorstore.similarity_search_with_score(vuln, k=k)
-        current_mapping = CweMapping(vulnerability=vuln, cwe_ids=[], docs=[], scores=[])
-        if results:
-            for result in results:
-                current_mapping.cwe_ids.append(result[0].metadata["CWE_ID"])
-                current_mapping.scores.append(result[1])
-                current_mapping.docs.append(result[0])
-        mapping_results.append(current_mapping)
-    return mapping_results
-
-
 # embeddings = OllamaEmbeddings(model="mxbai-embed-large", base_url=BASE_URL)
-print("loading cwe data...")
-cwe_data = load_cwe_data("cwe_dict_clean.json")
-print("building cwe vectorstore...")
-store_cwe = build_cwe_vectorstore(cwe_data, embeddings)
+store_cwe = get_cwe_vectorstore(embeddings)
+if store_cwe is None:
+    print("CWE vector db not found. Please build the db first.")
+    exit()
+
 for i in response_parsed:
     print(f"[-] vuln identified: {i}")
 
@@ -175,13 +196,13 @@ for mapping in res:
     You are a threat intelligence analyst.
     You have previously identified the following vulnerability:
     {mapping.vulnerability}
-    
+
     Based on the context provided, identify the best fitting CWE ID that describes the vulnerability.
     Format your response according to the prompt.
     """
 
     response_cwe = rag_chain_cwe.invoke({"input": question_cwe})
-    print(f"response from model:\n{response_cwe["answer"]}")
+    print(f"response from model:\n{response_cwe['answer']}")
     print("--------------------\n\n")
 
 # this does not work as well, prone to some hallucination
