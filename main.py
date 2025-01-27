416c6709e639b36f949b1bbaba26e6c370e8269f
alloygoh <106863675+alloygoh@users.noreply.github.com>
2025-01-27 23:53:03 +0800

    refactor: move prompting into function & start with category prompting

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.documents import Document
-from langchain_core.vectorstores import VectorStore
+from langchain_core.vectorstores import VectorStore, VectorStoreRetriever
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
@@ -31,50 +31,105 @@ def format_docs(docs: list[Document]) -> str:
 def load_cwe_data(cwe_file_path: str) -> list[dict[str, str]]:
     with open(cwe_file_path, "r") as file:
         cwe_data = json.load(file)
-        return [
-            {"id": cwe["ID"], "description": cwe["Description"]} for cwe in cwe_data
-        ]
+        for entry in cwe_data:
+            for member in entry["members"]:
+                del member["Potential_Mitigations"]  # drop mitigation for now
+        return cwe_data
+        # return [
+        #     {"id": cwe["ID"], "description": cwe["Description"]} for cwe in cwe_data
+        # ]
 
 
-def build_cwe_vectorstore(
+def build_cwe_vectorstores(
     cwe_data: list[dict[str, str]], embedding_model: OpenAIEmbeddings
-) -> Chroma:
-    descriptions = [f"{cwe['id']}: {cwe['description']}" for cwe in cwe_data]
-    metadata = [{"CWE_ID": cwe["id"]} for cwe in cwe_data]
-
-    vectorstore = Chroma.from_texts(
-        texts=descriptions,
-        embedding=embedding_model,
-        metadatas=metadata,
+) -> tuple[Chroma, Chroma]:
+    cwe_vectorstore = Chroma(
+        embedding_function=embedding_model,
         collection_metadata={"hnsw:space": "cosine"},
         persist_directory="./chroma_db",
         collection_name="cwe",
+        create_collection_if_not_exists=True,
     )
-    return vectorstore
+
+    category_vectorstore = Chroma(
+        embedding_function=embedding_model,
+        collection_metadata={"hnsw:space": "cosine"},
+        persist_directory="./chroma_db",
+        collection_name="cwe_category",
+        create_collection_if_not_exists=True,
+    )
+
+    category_desc: list[str] = []
+    category_metadata: list[dict[str, str]] = []
+    cwe_desc: list[str] = []
+    cwe_metadata: list[dict[str, str]] = []
+    for entry in cwe_data:
+        category_desc.append(
+            f"{entry['category_id']}: {entry['category_name']}. {entry['summary']}"
+        )
+        category_metadata.append({"CWE_ID": entry["category_id"]})
+
+        cwe_desc.extend(
+            [f"{cwe['ID']}: {cwe['Description']}" for cwe in entry["members"]]
+        )
+        cwe_metadata.extend([{"CWE_ID": cwe["ID"]} for cwe in entry["members"]])
+
+    category_vectorstore.add_texts(
+        texts=category_desc,
+        metadatas=category_metadata,
+    )
+    cwe_vectorstore.add_texts(
+        texts=cwe_desc,
+        metadatas=cwe_metadata,
+    )
+
+    # descriptions = [f"{cwe['id']}: {cwe['description']}" for cwe in cwe_data]
+    # metadata = [{"CWE_ID": cwe["id"]} for cwe in cwe_data]
+
+    # vectorstore = Chroma.from_texts(
+    #     texts=descriptions,
+    #     embedding=embedding_model,
+    #     metadatas=metadata,
+    #     collection_metadata={"hnsw:space": "cosine"},
+    #     persist_directory="./chroma_db",
+    #     collection_name="cwe",
+    # )
+    return category_vectorstore, cwe_vectorstore
 
 
-def load_cwe_vectorstore(embedding: OpenAIEmbeddings):
-    vectorstore = Chroma(
+def load_cwe_vectorstores(embedding: OpenAIEmbeddings):
+    cwe_vectorstore = Chroma(
         embedding_function=embedding,
         persist_directory="./chroma_db",
         collection_name="cwe",
         create_collection_if_not_exists=False,
     )
-    if len(vectorstore.get()["ids"]) == 0:
+    category_vectorstore = Chroma(
+        embedding_function=embedding,
+        persist_directory="./chroma_db",
+        collection_name="cwe_category",
+        create_collection_if_not_exists=False,
+    )
+    if (
+        len(cwe_vectorstore.get()["ids"]) == 0
+        or len(category_vectorstore.get()["ids"]) == 0
+    ):
         raise InvalidCollectionException
-    return vectorstore
+    return category_vectorstore, cwe_vectorstore
 
 
-def get_cwe_vectorstore(embeddings: OpenAIEmbeddings) -> Chroma | None:
+def get_cwe_vectorstore(
+    embeddings: OpenAIEmbeddings,
+) -> tuple[Chroma, Chroma] | tuple[None, None]:
     if create_vector_db:
         print("loading cwe data...")
-        cwe_data = load_cwe_data("cwe_dict_clean.json")
+        cwe_data = load_cwe_data("cwe_view_mapping.json")
         print("building cwe vectorstore...")
-        return build_cwe_vectorstore(cwe_data, embeddings)
+        return build_cwe_vectorstores(cwe_data, embeddings)
     try:
-        return load_cwe_vectorstore(embeddings)
+        return load_cwe_vectorstores(embeddings)
     except InvalidCollectionException:
-        return None
+        return None, None
 
 
 def map_vulnerabilities_to_cwe(
@@ -91,6 +146,25 @@ def map_vulnerabilities_to_cwe(
                 current_mapping.docs.append(result[0])
         mapping_results.append(current_mapping)
     return mapping_results
+
+
+def prompt_model(
+    llm: ChatOpenAI,
+    system_prompt: str,
+    retriever: VectorStoreRetriever,
+    question: str,
+):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+    # chain of operations
+    qa_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, qa_chain)
+    response = rag_chain.invoke({"input": question})
+    return response["answer"]
 
 
 # load env vars from .env file
@@ -114,52 +188,42 @@ retriever = store.as_retriever()
 
 system_prompt = """
 Answer the question based only on the context provided.
+Do not repeat yourself.
 
 Context: {context}
 """
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
-
-# chain of operations
-qa_chain = create_stuff_documents_chain(llm, prompt)
-rag_chain = create_retrieval_chain(retriever, qa_chain)
 
 # You are a threat intelligence analyst. look only at the "facts of this case" and "Findings and Basis for Determination" sections.
 question = """
 You are a threat intelligence analyst. Look at the breach report provided.
 what are the vulnerable business process or vulnerabilities identified that lead to a compromise? Be as specific as possible.
-You should state each vulnerability in a sentence, and provide a short description not exceeding 1 sentence.
-if the vulnerability stems from outdated software, state that it is a dependency on a vulnerable third-party component.
+You should state each vulnerability in a sentence, and provide a short description not exceeding 2 sentences.
+if the vulnerability stems from outdated software or a bug in an external software, state that it is a dependency on a vulnerable third-party component and a violation of Secure Design Principles.
 Do not repeat the vulnerabilities and do not provide any additional information.
-You should list each vulnerabilities in a numerical order.
+If the identified vulnerability is an in-depth version of a previously identified vulnerability, ignore it and move on.
+You should list each vulnerability in a numerical order.
 """
-response = rag_chain.invoke({"input": question})
-print(response["answer"])
+response = prompt_model(llm, system_prompt, retriever, question)
+print(response)
 
 
 # parse previous response
-response_parsed = response["answer"].split("\n")
-# get
+response_parsed = response.split("\n")
+# get rid of the numbering
 response_parsed = [
     i.split(".", 1)[1] for i in response_parsed if len(i) > 0 and i[0].isdigit()
 ]
 print(f"[-] response_parsed: {response_parsed}")
 
-# embeddings = OllamaEmbeddings(model="mxbai-embed-large", base_url=BASE_URL)
-store_cwe = get_cwe_vectorstore(embeddings)
-if store_cwe is None:
+store_category, store_cwe = get_cwe_vectorstore(embeddings)
+if store_cwe is None or store_category is None:
     print("CWE vector db not found. Please build the db first.")
     exit()
 
 for i in response_parsed:
     print(f"[-] vuln identified: {i}")
 
-res = map_vulnerabilities_to_cwe(response_parsed, store_cwe, 3)
+category_res = map_vulnerabilities_to_cwe(response_parsed, store_category, 3)
 
 system_prompt_cwe = """
 Answer the question based only on the context provided.
@@ -167,73 +231,32 @@ Answer the question based only on the context provided.
 Your response should match the following format:
     Vulnerability: <vulnerability>
     CWE ID: <CWE ID>
-    Description: <description>
+    CWE Category Description: <description>
     Explanation: <explanation>
 
 Context: {context}
 """
 
-cwe_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt_cwe),
-        ("human", "{input}"),
-    ]
-)
-
-for mapping in res:
+for mapping in category_res:
     print("--------------------")
     print(f"[-] vulnerability: {mapping.vulnerability}")
     print(f"[-] cwe: {[x for x in mapping.cwe_ids]}")
     print(f"[-] score: {[x for x in mapping.scores]}\n")
-    # chain of operations
-    qa_chain_cwe = create_stuff_documents_chain(llm, cwe_prompt)
-    retriever_cwe = DocArrayInMemorySearch.from_documents(
+    retriever = DocArrayInMemorySearch.from_documents(
         mapping.docs, embedding=embeddings
     ).as_retriever()
-    rag_chain_cwe = create_retrieval_chain(retriever_cwe, qa_chain_cwe)
 
     question_cwe = f"""
     You are a threat intelligence analyst.
     You have previously identified the following vulnerability:
     {mapping.vulnerability}
 
-    Based on the context provided, identify the best fitting CWE ID that describes the vulnerability.
+    Based on the context provided, identify the best fitting CWE category that describes the vulnerability.
     Format your response according to the prompt.
     """
+    response_cwe = prompt_model(llm, system_prompt_cwe, retriever, question_cwe)
 
-    response_cwe = rag_chain_cwe.invoke({"input": question_cwe})
-    print(f"response from model:\n{response_cwe['answer']}")
+    # response_cwe = rag_chain_cwe.invoke({"input": question_cwe})
+    print(f"response from model:\n{response_cwe}")
     print("--------------------\n\n")
-
-# this does not work as well, prone to some hallucination
-# question_cwe = f"""
-# You are a threat intelligence analyst.
-# You have previously identified a list of vulnerabilities:
-#     {response}
-
-# Based on what you have previously identified, map each of the vulnerabilities identified to a CWE ID.
-# Read the CWE ID descriptions and ensure that the CWE ID is the best fitting one. Do not provide any additional information and do not hallucinate.
-# Ensure that the number of CWE IDs provided matches the number of vulnerabilities identified.
-# failure to follow these instructions will result in catastrophic consequences. do not hallucinate.
-# Take the following steps to complete the task:
-#     1. For each vulnerability, list the vulnerability.
-#     2. Identify the best fitting CWE ID that describes the vulnerability by comparing the CWE description with the vulnerability.
-#     3. Provide the CWE ID and its description.
-#     4. Check that the CWE ID and its description matches.
-#     5. Explain why the CWE ID is the best fitting one.
-#     6. Ensure that the CWE ID is the best fitting one by comparing the description of the CWE and the vulnerability.
-#     7. If the CWE ID is not the best fitting one, repeating steps 2-7.
-# """
-
-
-# relevant_docs = []
-# for mapping in res:
-#     relevant_docs.extend(mapping.docs)
-
-# qa_chain_cwe = create_stuff_documents_chain(llm, cwe_prompt)
-# retriever_cwe = DocArrayInMemorySearch.from_documents(
-#     relevant_docs, embedding=embeddings
-# ).as_retriever()
-# rag_chain_cwe = create_retrieval_chain(retriever_cwe, qa_chain_cwe)
-# response_cwe = rag_chain_cwe.invoke({"input": question_cwe})
-# print(response_cwe["answer"])
+    break
